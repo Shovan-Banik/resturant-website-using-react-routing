42168d259918b675f51f7545e2769fb84a9503a2
Shovan Banik <53087831+Shovan-Banik@users.noreply.github.com>
2023-04-04 19:38:10 +0600

    fetch data dynamically and show its details

--- src/component/MealDetails.jsx
+++ src/component/MealDetails.jsx
@@ -4,10 +4,24 @@ import { useLoaderData } from 'react-router-dom';
 const MealDetails = () => {
     const meal=useLoaderData();
     const {meals}=meal;
-    console.log(meals[0]);
+    const mealDetail=meals[0]
+    console.log(mealDetail);
     return (
-        <div>
-            
+        <div className='px-24 py-12'>
+             <h2 className='text-5xl font-semibold text-center'>{mealDetail.strMeal}</h2>
+            <div className='flex p-8 j justify-center gap-24 border-4 border-pink-800 rounded-lg mt-8'>
+            <div className='border-4 border-gray-500 p-4 w-full'>
+                <img className='h-96 rounded-lg mx-auto' src={mealDetail.strMealThumb} alt="food" />
+            </div>
+            <div className='border-4 border-gray-500 p-4 w-full text-center'>
+                <h5 className='text-2xl font-semibold text-center'>Category: {mealDetail.strCategory}</h5>
+                <h5 className='text-2xl font-semibold text-center'>Area: {mealDetail.strArea}</h5>
+                <h5 className='text-2xl font-semibold text-center'>Size: {mealDetail.strMeasure2}</h5>
+                <h5 className='text-2xl font-semibold text-center'>Tags: {mealDetail.strTags}</h5>
+                <h5 className='font-semibold text-center'>Description: {mealDetail.strInstructions.slice(0,250)}....</h5>
+                <button className='bg-pink-600 text-white hover:bg-pink-900 p-2 w-36 my-2 rounded-xl font-bold'>Add to Cart</button>
+            </div>
+            </div>
         </div>
     );
 };
